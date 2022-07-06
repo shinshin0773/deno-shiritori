989e1b647bfb8906ac8cb32f303cee575656a28f
Shin <zhangzeshenyilang@gmail.com>
2022-07-06 15:32:13 +0900

    カスタマイズ（単語チェック・ランダムに決まる・「ん」でゲーム終了・ひらがな以外入力できない）

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 import {serve} from "https://deno.land/std@0.138.0/http/server.ts"
 import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
 
-let previousWord = "しりとり";
+//
+// const WordList = [
+//   "きょり",
+//   "かいきゅう",
+//   "ぎゃくすう",
+//   "すいちょくにとうぶんせん",
+//   "せいさんかくすい",
+//   "どすうぶんぷひょう",
+//   "ねじれのいち",
+//   "ちぇばのていり",
+//   "ふぇるまーのさいしゅうていり",
+//   "へくたーる",
+//   "ぴたごらすしき",
+//   "ぜったいふとうしき",
+//   "くらめるのこうしき",
+//   "ふーりえかいきゅう"
+// ];
+// let result = "";
+// result = WordList[Math.floor(Math.random() * WordList.length)];
+
+
+
+let previousWord = randomWord();
+const list = [];
 
 console.log("Listening on http://localhost:8000");
 serve(async(req) => {
@@ -11,16 +34,39 @@ serve(async(req) => {
   if(req.method === "GET" && pathname === "/shiritori"){
     return new Response(previousWord);
   }
+
   if(req.method === "POST" && pathname === "/shiritori"){
     const requestJson = await req.json();
     const nextWord = requestJson.nextWord;
-    // ブラウザから送られてきた単語が前の単語の最後の文字で始まっているかどうかチエックしておかしければ更新しない
+
+    //ひらがなチェック
+    if(!hiraganaCheck(nextWord)) {
+      return new Response("ひらがなのみで入力してください", {
+        status: 400
+      });
+    }
+
+    //同じ文字チェック
+    if(list.includes(nextWord)) {
+      return new Response("一度入力した単語は使用できません",{status: 400});
+    }
+
+    //「ん」が出たらゲーム終了
+    if (nextWord.charAt(nextWord.length - 1) == "ん") {
+      previousWord = randomWord();
+      return new Response(previousWord);
+    }
+
+    // 入力チェック
     if (
       nextWord.length > 0 &&
       previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)
     ) {
       return new Response("前の単語に続いていません。", { status: 400 });
     }
+
+
+    list.push(nextWord);
     previousWord = nextWord;
     return new Response(previousWord);
   }
@@ -32,3 +78,37 @@ serve(async(req) => {
     enableCors: true,
   });
 });
+
+function randomWord() {
+  const wordList = [
+  "きょり",
+  "かいきゅう",
+  "ぎゃくすう",
+  "すいちょくにとうぶんせん",
+  "せいさんかくすい",
+  "どすうぶんぷひょう",
+  "ねじれのいち",
+  "ちぇばのていり",
+  "ふぇるまーのさいしゅうていり",
+  "へくたーる",
+  "ぴたごらすしき",
+  "ぜったいふとうしき",
+  "くらめるのこうしき",
+  "ふーりえかいきゅう"
+  ];
+  let result = "";
+  result = wordList[Math.floor(Math.random() * wordList.length)];
+  console.log(result);
+  return result;
+}
+
+
+
+function hiraganaCheck(str){
+  str = (str==null)?"":str;
+  if(str.match(/^[ぁ-んー　]*$/)){    //"ー"の後ろの文字は全角スペースです。
+    return true;
+  }else{
+    return false;
+  }
+}
